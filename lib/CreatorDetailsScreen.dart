49c608446c856de8e4abbbbd9db85f7254b029ec
LencoDigitexer <addminuser258@gmail.com>
2023-12-25 01:52:17 +0300

    💾 Cache image in post (Fix 429 HTTP CODE)

--- lib/CreatorDetailsScreen.dart
+++ lib/CreatorDetailsScreen.dart
@@ -1,6 +1,11 @@
 import 'dart:convert';
+import 'dart:typed_data';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:path_provider/path_provider.dart';
+
 
 class CreatorDetailsScreen extends StatefulWidget {
   final Map<String, dynamic> creator;
@@ -36,6 +41,18 @@ class _CreatorDetailsScreenState extends State<CreatorDetailsScreen> {
     }
   }
 
+  Future<void> saveImageLocally(String imageUrl) async {
+    try {
+      final response = await http.get(Uri.parse(imageUrl));
+      final bytes = response.bodyBytes;
+      final appDir = await getApplicationDocumentsDirectory();
+      final file = File('${appDir.path}/cached_images/${DateTime.now().millisecondsSinceEpoch}.png');
+      await file.writeAsBytes(bytes);
+    } catch (e) {
+      print('Error saving image: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,12 +69,21 @@ class _CreatorDetailsScreenState extends State<CreatorDetailsScreen> {
                 itemCount: posts.length,
                 itemBuilder: (context, index) {
                   final post = posts[index];
+                  final imageUrl = 'https://coomer.su${post['file']['path']}';
+
+                  // Save the image locally when it is first loaded
+                  saveImageLocally(imageUrl);
+
                   return Card(
                     margin: EdgeInsets.symmetric(vertical: 8),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Image.network('https://coomer.su${post['file']['path']}'),
+                        CachedNetworkImage(
+                          imageUrl: imageUrl,
+                          placeholder: (context, url) => CircularProgressIndicator(),
+                          errorWidget: (context, url, error) => Icon(Icons.error),
+                        ),
                         Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: Column(
